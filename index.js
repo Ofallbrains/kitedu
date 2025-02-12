d4cbc742915c7ee957d967e0f8bdef18b934b5ff
Ofallbrains <ofallbrains@gmail.com>
2025-02-12 15:46:45 +0200

    kitedu website cloning exercise

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const clickMenu = document.getElementById("menu-toogle")
-const mainMenu = document.getElementById("menu")
-const menuIcon = document.getElementById("menu-icon")
+// const clickMenu = document.getElementById("menu-toogle")
+// const mainMenu = document.getElementById("menu")
+// const menuIcon = document.getElementById("menu-icon")
 
-clickMenu.addEventListener('click', () => {
-  console.log('hy')
-  mainMenu.classList.toggle('translate-x-[100%]')
+// clickMenu.addEventListener('click', () => {
+//   console.log('hy')
+//   mainMenu.classList.toggle('translate-x-[100%]')
 
-  if (mainMenu.classList.contains('translate-x-[100%]')) {
-    menuIcon.setAttribute('name', 'menu-outline')
-  } else {
-    menuIcon.setAttribute('name', 'close-outline')
-  }
-})
+//   if (mainMenu.classList.contains('translate-x-[100%]')) {
+//     menuIcon.setAttribute('name', 'menu-outline')
+//   } else {
+//     menuIcon.setAttribute('name', 'close-outline')
+//   }
+// })
 
-//logic for pages
-const pages = document.getElementById('pages')
-let currentIndex = 0;
-const totalPages = 3;
+// //logic for pages
+// const pages = document.getElementById('pages')
+// let currentIndex = 0;
+// const totalPages = 3;
 
-function showPage(index) {
-  pages.style.transform = `translateX(-${index * 100}%)`
-}
+// function showPage(index) {
+//   pages.style.transform = `translateX(-${index * 100}%)`
+// }
 
-function nextPage() {
-  currentIndex = (currentIndex + 1) % totalPages
-  showPage(currentIndex)
-}
+// function nextPage() {
+//   currentIndex = (currentIndex + 1) % totalPages
+//   showPage(currentIndex)
+// }
 
-function prevPage() {
-  currentIndex = (currentIndex - 1 + totalPages) % totalPages;
-  showPage(currentIndex)
-}
+// function prevPage() {
+//   currentIndex = (currentIndex - 1 + totalPages) % totalPages;
+//   showPage(currentIndex)
+// }
 
-setInterval(nextPage, 1000) 
+// setInterval(nextPage, 1000) 
